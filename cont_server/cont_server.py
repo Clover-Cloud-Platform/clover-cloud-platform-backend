d2e8590f5a59f08a8f3938b64317d76dcc876ab4
Daniil <101560362+Astel-d@users.noreply.github.com>
2022-11-25 23:17:48 +0300

    Fix

--- cont_server/cont_server.py
+++ cont_server/cont_server.py
@@ -14,20 +14,22 @@ sio.emit('ContConnected', cont_code)
 
 @sio.on('RunGazebo')
 def run_gazebo(data):
-    aruco_pose = []
+    aruco_poses = []
     aruco_svg = []
     aruco_map = []
-    root = ET.parse(r'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/aruco_cmit_txt/aruco_model.sdf').getroot()
+    root = ET.parse(r'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/aruco_cmit_txt/aruco_model.sdf')\
+        .getroot()
 
     for type_tag in root.findall('.//pose'):
-        aruco_pose.append(type_tag.text[0:7].split(' ').append('1.57079632679'))
-
+        aruco_poses.append(type_tag.text[0:7].split().append('1.57079632679'))
+        
     for type_tag in root.findall('.//name'):
-        aruco_name = f'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/aruco_cmit_txt/materials/textures/{type_tag.text.replace("/", "_")}.png'
+        aruco_name = f'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/aruco_cmit_txt/materials/textures/' \
+                     f'{type_tag.text.replace("/", "_")}.png'
         svg_image = main(aruco_name)
         aruco_svg.append(svg_image)
 
-    for index in range (len(aruco_pose)):
-        aruco_map.append({'image': aruco_svg[index], 'position': aruco_pose[index]})
-    print(aruco_map)
+    for index in range(len(aruco_poses)):
+        aruco_map.append({'image': aruco_svg[index], 'position': aruco_poses[index]})
+        
     sio.emit('GazeboModels', {'aruco_map': aruco_map, 'user_sid': data['user_sid']})
