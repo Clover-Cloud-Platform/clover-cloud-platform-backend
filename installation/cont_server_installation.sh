c4321d9f4b0bedacd9b16d6d1455f687228d8c9e
Daniil <101560362+Astel-d@users.noreply.github.com>
2023-05-01 00:04:38 +0300

    Create cont_server_installation.sh

---
+++ installation/cont_server_installation.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+echo "Enter your main server address"
+read adress
+
+sudo apt install npm
+pip3 install python-socketio
+pip3 install Pillow
+
+cd ~
+
+mkdir CloverCloudPlatform
+
+cd CloverCloudPlatform
+
+npm i os node-pty socket.io-client ansi-to-html child_process -g
+
+mkdir cont_server
+mkdir user_objects
+mkdir default_models
+
+wget https://raw.githubusercontent.com/Clover-Cloud-Platform/clover-cloud-platform-backend/main/cont_server/cont_server.py
+
+wget https://raw.githubusercontent.com/Clover-Cloud-Platform/clover-cloud-platform-backend/main/cont_server/stream_led.py
+
+wget https://raw.githubusercontent.com/Clover-Cloud-Platform/clover-cloud-platform-backend/main/cont_server/stream_telemetry.py
+
+wget https://raw.githubusercontent.com/Clover-Cloud-Platform/clover-cloud-platform-backend/main/cont_server/terminal.js
+
+wget https://raw.githubusercontent.com/Astel-d/PngToSvgForCloverCloud/master/pngtosvg.py
+
+sed -i "s,your local adress,${adress}," cont_server.py
+sed -i "s,your local adress,${adress}," stream_led.py
+sed -i "s,your local adress,${adress}," stream_telemetry.py
+sed -i "s,yor local adress,${adress}," terminal.js
+
+cd default_models 
+mkdir default_cube
+cd default_cube 
+
+wget https://raw.githubusercontent.com/Clover-Cloud-Platform/clover-cloud-platform-backend/main/cont_server/default_models/default_cube/model.config
+
+wget https://raw.githubusercontent.com/Clover-Cloud-Platform/clover-cloud-platform-backend/main/cont_server/default_models/default_cube/model.sdf
+
+
+echo "\e[32mInstallation sucsess\e[0m"
