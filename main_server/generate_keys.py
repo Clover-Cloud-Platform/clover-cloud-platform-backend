1ce2b2e813c20659b26ff3c25ac78cdf1cfb4b85
Daniil <101560362+Astel-d@users.noreply.github.com>
2023-05-12 17:08:13 +0300

    Delete generate_keys.py

--- main_server/generate_keys.py
+++
@@ -1,9 +0,0 @@
-import rsa
-
-(pubkey, privkey) = rsa.newkeys(2048)
-
-with open('public.pem', 'w+') as f:
-    f.write(pubkey.save_pkcs1().decode('utf-8'))
-
-with open('private.pem', 'w+') as f:
-    f.write(privkey.save_pkcs1().decode('utf-8'))
