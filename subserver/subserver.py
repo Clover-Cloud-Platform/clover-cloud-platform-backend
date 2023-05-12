8676a187844de17c641881edfaa816e1796c6973
Daniil <101560362+Astel-d@users.noreply.github.com>
2023-05-12 17:14:44 +0300

    Update subserver.py
    Several new sockets and bug fixes

--- subserver/subserver.py
+++ subserver/subserver.py
@@ -1,9 +1,11 @@
 import json
 from pylxd import Client
 import socketio
+import random
+import string
 
 sio = socketio.Client()
-sio.connect('your main server adress')
+sio.connect('main_server_address')
 client = Client()
 
 with open("db.json", "r") as read_file:
@@ -41,7 +43,7 @@ def create_instance(data):
                                  'cont_list': data['cont_list'], 'server_data': server_data})
 
 
-@sio.on('GetRunningInstances')
+@sio.on('Instances')
 def running(data):
     cont_list = data['cont_list']
     print(cont_list)
@@ -52,7 +54,7 @@ def running(data):
             cont_list[cont][1] = True
         else:
             pass
-    sio.emit('RunningInstances', {'username': data['username'], 'cont_list': cont_list,
+    sio.emit('RunningInstances', {'cont_list': cont_list,
                                   'user_sid': data['user_sid']})
 
 
@@ -88,3 +90,59 @@ def delete(data):
 
         with open("db.json", "w") as write_file:
             json.dump(server_data, write_file)
+
+
+@sio.on('CreateNewTemplate')
+def create_new_template(data):
+
+    cont_code = "c" + ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(6))
+
+    client.instances.create({'name': cont_code, 'source': {'type': 'copy', 'source': data['instanceID']}})
+    data['instanceID'] = cont_code
+    data['server_data'] = server_data
+
+    server_data['cont_list'].append(cont_code)
+
+    with open("db.json", "w") as write_file:
+        json.dump(server_data, write_file)
+
+    sio.emit('TemplateCreated', data)
+
+
+@sio.on('InstallTemplate')
+def install_template(data):
+    global server_data
+
+    client.instances.get(data['oldID']).stop(wait=True)
+    client.instances.get(data['oldID']).delete(wait=True)
+
+    client.instances.create({'name': data['oldID'], 'source': {'type': 'copy', 'source': data['newID']}}, wait=True)
+
+    server_data['cont_list'].append(data['oldID'])
+    with open("db.json", "w") as write_file:
+        json.dump(server_data, write_file)
+
+    client.instances.get(data['oldID']).start(wait=True)
+
+    sio.emit('TemplateInstalled', {'server_data': server_data, 'user_sid': data['sid']})
+
+
+@sio.on('DeleteTemplate')
+def delete_template(data):
+    global server_data
+
+    client.instances.get(data).delete()
+    server_data['cont_list'].remove(data)
+
+    sio.emit('UpdateServerData', server_data)
+
+    with open("db.json", "w") as write_file:
+        json.dump(server_data, write_file)
+
+
+@sio.on('RevertToInitial')
+def revert_to_initial(data):
+    client.instances.get(data).stop(wait=True)
+    client.instances.get(data).delete(wait=True)
+
+    client.instances.create({'name': data, 'source': {'type': 'copy', 'source': 'clover0'}}, wait=True)
