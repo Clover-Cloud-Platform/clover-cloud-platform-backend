35e04523629ba780a938de0d43dc53f16d5a35bf
Daniil <101560362+Astel-d@users.noreply.github.com>
2023-05-12 17:18:11 +0300

    bug fix

--- cont_server/cont_server.py
+++ cont_server/cont_server.py
@@ -9,7 +9,7 @@ import xml.etree.ElementTree as ET
 import socketio
 
 sio = socketio.Client()
-sio.connect('your local adress')
+sio.connect('main_server_address')
 
 cont_code = subprocess.run("hostname", shell=True, stdout=subprocess.PIPE, text=True).stdout.replace("\n", '')
 sio.emit('ContConnected', cont_code)
@@ -27,15 +27,6 @@ def get_gazebo_state(data):
     else:
         sio.emit("GazeboStateRes",
                  {'gazebo_state': False, 'user_sid': data['user_sid'], 'cont_code': data['cont_code']})
-
-
-@sio.on('StopGazebo')
-def stop_gazebo(data):
-    print('Stop')
-    sio.emit('DebugOutput', subprocess.run("pkill -f 'roslaunch clover_simulation simulator.launch'", shell=True, stderr=subprocess.PIPE, text=True).stderr)
-    sio.emit('DebugOutput', subprocess.run("pkill -f '/usr/bin/python3.8 /home/ubuntu/PngToSvg/stream_telemetry.py'", shell=True, stderr=subprocess.PIPE, text=True).stderr)
-    sio.emit('DebugOutput', subprocess.run("pkill -f '/usr/bin/python3.8 /home/ubuntu/PngToSvg/stream_led.py'", shell=True, stderr=subprocess.PIPE, text=True).stderr)
-    sio.emit('GazeboStopped', data)
 
 
 @sio.on('GetFiles')
@@ -113,7 +104,15 @@ def write_file(data):
 @sio.on('EditMarker')
 def edit_marker(data):
     print('Marker', data)
+    line_number = 0
     aruco_id = re.findall(r'\d+', data["aruco_name"])[1]
+
+    with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'r') as map_txt:
+        lines = map_txt.readlines()
+        for line in lines:
+            if line.split('\t')[0] == aruco_id:
+                break
+            line_number += 1
 
     if data['image'] is not None:
         print(data['aruco_name'].replace("aruco_marker_", ""))
@@ -126,9 +125,9 @@ def edit_marker(data):
     if data['size'] is not None:
         with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'r') as map_txt:
             lines = map_txt.readlines()
-            line = lines[int(aruco_id)].split('\t')
+            line = lines[line_number].split('\t')
             line[1] = str(data['size'])
-            lines[int(aruco_id)] = '\t'.join(line)
+            lines[line_number] = '\t'.join(line)
 
         with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'w') as map_txt:
             map_txt.writelines(lines)
@@ -137,9 +136,9 @@ def edit_marker(data):
     if data['position'] is not None:
         with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'r') as map_txt:
             lines = map_txt.readlines()
-            line = lines[int(aruco_id)].split('\t')
+            line = lines[line_number].split('\t')
             line[2], line[3] = str(data['position'][0]), str(data['position'][1])
-            lines[int(aruco_id)] = '\t'.join(line)
+            lines[line_number] = '\t'.join(line)
 
         with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'w') as map_txt:
             map_txt.writelines(lines)
@@ -149,9 +148,9 @@ def edit_marker(data):
 
         with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'r') as map_txt:
             lines = map_txt.readlines()
-            line = lines[int(aruco_id)].split('\t')
+            line = lines[line_number].split('\t')
             line[0] = str(data['marker_id'])
-            lines[int(aruco_id)] = '\t'.join(line)
+            lines[line_number] = '\t'.join(line)
 
         with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'w') as map_txt:
             map_txt.writelines(lines)
@@ -188,9 +187,59 @@ def edit_marker(data):
         )
 
 
+@sio.on('AddMarker')
+def add_marker(data):
+    aruco_id = re.findall(r'\d+', data["name"])[1]
+
+    with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'a') as map_txt:
+        map_txt.write(f'\n{aruco_id}	0.33	0.0	-1.0	0	0	0	0')
+
+    subprocess.run(""
+                   "/bin/bash -c 'source /home/ubuntu/catkin_ws/devel/setup.bash; "
+                   "rosrun clover_simulation aruco_gen --single-model "
+                   "--source-world='/home/ubuntu/catkin_ws/src/clover/clover_simulation/resources/worlds/clover"
+                   ".world' '/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt' > "
+                   "'/home/ubuntu/catkin_ws/src/clover/clover_simulation/resources/worlds/clover_aruco.world''",
+                   shell=True)
+
+
+@sio.on('DeleteMarker')
+def delete_marker(data):
+    x = 0
+    aruco_id = re.findall(r'\d+', data["name"])[1]
+
+    with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'r') as map_txt:
+        lines = map_txt.readlines()
+        for line in lines:
+            if line.split('\t')[0] == aruco_id:
+                lines.pop(x)
+            x += 1
+
+    with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'w') as map_txt:
+        map_txt.writelines(lines)
+        map_txt.close()
+
+    if data["name"] in user_objects['pictures']:
+        user_objects['pictures'].remove(data['name'])
+
+        os.remove(fr'/home/ubuntu/.gazebo/models/aruco_cmit_txt/materials/textures/'
+                  fr'{data["name"].replace("aruco_marker_", "")}.png')
+
+        with open("/home/ubuntu/CloverCloudPlatform/user_objects.json", "w") as write_file:
+            json.dump(user_objects, write_file)
+
+    subprocess.run(""
+                   "/bin/bash -c 'source /home/ubuntu/catkin_ws/devel/setup.bash; "
+                   "rosrun clover_simulation aruco_gen --single-model "
+                   "--source-world='/home/ubuntu/catkin_ws/src/clover/clover_simulation/resources/worlds/clover"
+                   ".world' '/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt' > "
+                   "'/home/ubuntu/catkin_ws/src/clover/clover_simulation/resources/worlds/clover_aruco.world''",
+                   shell=True)
+
+
 @sio.on('AddCube')
 def add_cube(data):
-    dir_name = str(len(user_objects['models']))
+    dir_name = data
     os.mkdir(fr'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/{dir_name}')
     path_to_sdf = fr'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/{dir_name}/model.sdf'
     path_to_config = fr'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/{dir_name}/model.config'
@@ -202,7 +251,7 @@ def add_cube(data):
                     path_to_config)
 
     user_objects['models'].append(
-        {'size': [1, 1, 1], 'position': [0, 1, 0.5], 'rotation': [0, 0, 0], 'color': 'Gray', 'colorHex': '#838383'}
+        {'cubeID': dir_name, 'size': [1, 1, 1], 'position': [0, 1, 0.5], 'rotation': [0, 0, 0], 'color': 'Gray', 'colorHex': '#838383'}
     )
 
     sdf_tree = ET.parse(path_to_sdf)
@@ -250,8 +299,12 @@ def edit_cube(data):
                 break
         tree.write(r'/home/ubuntu/catkin_ws/src/clover/clover_simulation/resources/worlds/clover_aruco.world')
         tree.write(r'/home/ubuntu/catkin_ws/src/clover/clover_simulation/resources/worlds/clover.world')
-        user_objects['models'][int(data['model_id'])]['position'] = data['position']
-        user_objects['models'][int(data['model_id'])]['rotation'] = data['rotation']
+
+        next(item for item in user_objects['models'] if item["cubeID"] == data['model_id'])['position'] \
+            = data['position']
+
+        next(item for item in user_objects['models'] if item["cubeID"] == data['model_id'])['rotation'] \
+            = data['rotation']
 
     if data['size'] is not None:
         print('scale')
@@ -265,7 +318,9 @@ def edit_cube(data):
             type_tag.text = ' '.join(map(str, data['size']))
 
         tree.write(fr'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/{data["model_id"]}/model.sdf')
-        user_objects['models'][int(data['model_id'])]['size'] = data['size']
+
+        next(item for item in user_objects['models'] if item["cubeID"] == data['model_id'])['size'] \
+            = data['size']
 
     if data['color'] is not None:
         tree = ET.parse(fr'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/{data["model_id"]}/model.sdf')
@@ -276,8 +331,29 @@ def edit_cube(data):
 
         tree.write(fr'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/{data["model_id"]}/model.sdf')
 
-        user_objects['models'][int(data['model_id'])]['color'] = data['color']
-        user_objects['models'][int(data['model_id'])]['colorHex'] = data['colorHex']
+        next(item for item in user_objects['models'] if item["cubeID"] == data['model_id'])['color'] \
+            = data['color']
+
+        next(item for item in user_objects['models'] if item["cubeID"] == data['model_id'])['colorHex'] \
+            = data['colorHex']
+
+    with open("/home/ubuntu/CloverCloudPlatform/user_objects.json", "w") as write_file:
+        json.dump(user_objects, write_file)
+
+
+@sio.on('DeleteCube')
+def delete_cube(data):
+    shutil.rmtree(fr'/home/ubuntu/catkin_ws/src/clover/clover_simulation/models/{data}')
+    world_tree = ET.parse(r'/home/ubuntu/catkin_ws/src/clover/clover_simulation/resources/worlds/clover_aruco.world')
+    world_root = world_tree.getroot()
+
+    for elem in world_root.findall(fr'.//include[uri="{data}"]'):
+        world_root.remove(elem)
+
+    world_tree.write(r'/home/ubuntu/catkin_ws/src/clover/clover_simulation/resources/worlds/clover_aruco.world')
+    world_tree.write(r'/home/ubuntu/catkin_ws/src/clover/clover_simulation/resources/worlds/clover.world')
+
+    next(item for item in user_objects['models'] if item["cubeID"] == data['model_id']).pop(int(data))
 
     with open("/home/ubuntu/CloverCloudPlatform/user_objects.json", "w") as write_file:
         json.dump(user_objects, write_file)
@@ -298,7 +374,7 @@ def run_gazebo(data):
         root = ET.fromstring(xml_string)
 
     for type_tag in root.findall('.//pose'):
-        aruco_poses.append(type_tag.text[0:7].split())
+        aruco_poses.append(type_tag.text.split()[0:2])
 
     with open(r'/home/ubuntu/catkin_ws/src/clover/aruco_pose/map/cmit.txt', 'r') as map_txt:
         lines = map_txt.readlines()
